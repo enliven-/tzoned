60084b093dfead8083d5ef941e8a72c54bf501c9
Viksit Arora <viksit@mit.edu>
2016-01-18 13:54:47 +0530

    adds user update endpoint; includes specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,33 @@ class UsersController < ApplicationController
   
   respond_to :json
 
+
   def show
     respond_with User.find(params[:id])
   end
 
+
   def create
     user = User.new(user_params)
+    
     if user.save
-      render json: user, status: 201
+      render json: user, status: 201, location: [user]
     else
       render json: { errors: user.errors }, status: 422
     end
   end
+
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: 200, location: [user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources  :users, only: [:show, :create]
+  resources  :users, only: [:show, :create, :update]
   
   get '/test', to: 'test#index'
 end
