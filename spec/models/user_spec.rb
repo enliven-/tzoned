a6a74e7e2bc4288e640af5121ff025dbb2f406b5
Viksit Arora <viksit@mit.edu>
2016-01-18 13:03:41 +0530

    adds basic specs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  it 'responds to attributes name, email and password (& confirmation)' do
+    user = User.new
+
+    expect(user).to respond_to(:name)
+    expect(user).to respond_to(:email)
+    expect(user).to respond_to(:password)
+    expect(user).to respond_to(:password_confirmation)
+  end
+
+
+  it 'has valid factories' do
+    user = FactoryGirl.build(:user)
+
+    expect(user).to be_valid
+    expect(user.save).to be_truthy
+  end
+
+
+  it 'should validate presence of name, email & password' do
+    user  = User.new
+
+    expect(user).to validate_presence_of(:name)
+    expect(user).to validate_presence_of(:email)
+    expect(user).to validate_presence_of(:password)
+  end
+
+
+  # it 'should validate case-insensitive uniqueness of email' do
+  #   user  = User.new
+
+  #   expect(user).to validate_uniqueness_of(:email).case_insensitive
+  # end
+
 end
