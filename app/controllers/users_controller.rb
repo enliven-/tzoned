551c5e921c77c7a20571cd288e64b52a72f21c04
Viksit Arora <viksit@mit.edu>
2016-01-18 13:57:26 +0530

    adds user destroy endpoint; adds basic spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    
+
     if user.save
       render json: user, status: 201, location: [user]
     else
@@ -30,6 +30,13 @@ class UsersController < ApplicationController
   end
 
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head 204
+  end
+
+  
   private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources  :users, only: [:show, :create, :update]
+  resources  :users, only: [:show, :create, :update, :destroy]
   
   get '/test', to: 'test#index'
 end
